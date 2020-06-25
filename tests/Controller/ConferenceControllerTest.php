b899ada4899320697d1e138a35a12bdd800a08a9
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-06-25 11:23:51 +0100

    Add unit testing

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+}
